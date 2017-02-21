233793591d3983bfe4b8a02c575b61aace22dea2
jahooligan50 <jefseaff@gmail.com>
2017-02-21 14:42:30 -0800

    new motor testing .ino file
    used to test pmos circuit

---
+++ testing/motor_pmos_test_2_21/motor_pmos_test_2_21.ino
@@ -0,0 +1,27 @@
+// Parallax Ping Ultrasonic Sensor test code
+// Axion Electronics
+// Kyle Lam, John Efseaff
+// 5V to 5V output, GND to GND, SIG to digital pin 7 (PWM)
+
+// this constant won't change.  It's the pin number
+// of the sensor's output:
+const int pingPin = 30;
+
+void setup() {
+  // initialize serial communication for serial monitor
+  Serial.begin(9600);
+    pinMode(pingPin, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(pingPin, HIGH);
+  delay(1000);
+  digitalWrite(pingPin, LOW);
+  delay(1000);
+  digitalWrite(pingPin, HIGH);
+
+  delay(1000);
+}
+
